d2e4dc2624e5055435b712e800b8d23afc129efa
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2012-03-23 13:18:18 +0530

    Removed django-addons

--- dorrie/urls.py
+++ dorrie/urls.py
@@ -49,5 +49,3 @@ if settings.STATIC_SERVE:
          {'document_root': settings.MEDIA_ROOT}),
     )
 
-if settings.ENABLE_ADDONS:
-    urlpatterns += patterns('', (r'', include('django_addons.urls')))
